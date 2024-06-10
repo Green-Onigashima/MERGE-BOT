b8a4a27c12e7e76d63dbd8318bbed9b2d413c205
Green-Onigashima <greenmorongetlostagain@gmail.com>
2024-06-10 09:35:01 +0530

    config.py

--- config.py
+++ config.py
@@ -2,16 +2,23 @@ import os
 
 
 class Config(object):
-    API_HASH =("0b691c3e86603a7e34aae0b5927d725a")
-    BOT_TOKEN = ("6850585424:AAH8A5F9FgvEua16inW0jKEDqWkFpfobScM")
-    TELEGRAM_API = ("25695562")
-    OWNER = ("1895952308")
-    OWNER_USERNAME = ("StupidBoi69")
-    PASSWORD = ("6342")
-    DATABASE_URL = ("mongodb+srv://jidap30478:jidap30478@cluster0.l7j63yf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    LOGCHANNEL = ("-1001788923244")  # Add channel id as -100 + Actual ID
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = os.environ.get("API_ID")
+    OWNER = os.environ.get("OWNER")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    PASSWORD = os.environ.get("PASSWORD")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    LOGCHANNEL = os.environ.get("LOGCHANNEL")
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = True
-    START_PIC = ("https://graph.org/file/b2e20c24139dd72dbac09.jpg")
+    START_PIC =  os.environ.get("START_PIC")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    TIMEOUT = os.environ.get("TIMEOUT")
+    SHORTENER_API = os.environ.get("SHORTENER_API")
+    SHORTENER_SITE =  os.environ.get("SHORTENER_SIE")
+    PAID_PROMOTIOM =  os.environ.get("PAID_PROMOTION")
+    
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
+    
